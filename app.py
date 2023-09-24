eefdd3b63d5686694f9ba3ac41680590eefb636f
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-09-24 23:00:09 +0300

    vielä muutoksia

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL", "postgresql:///maija")
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
 
 db.init_app(app)
 
