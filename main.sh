1e0c17bcd16e33b1910498f4f1881a8ee598cc08
Tamás Tóth <tamas.toth@helsinki.fi>
2023-09-26 17:49:51 +0300

    integrate with systemd, also rm psycopg2 as it was unavailable for aarch64 in Oracle's server

---
+++ main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SERVICE="vaittelyfoorumi"
+
+if [ "$1" == "systemd" ]; then
+    systemctl --user $2 "$SERVICE"
+elif [ "$1" == "update" ]; then
+    git fetch origin
+    if $(git pull | grep -q '^Updating'); then
+        systemctl --user daemon-reload
+        systemctl --user restart "$SERVICE"
+    fi
+else
+    source venv/bin/activate
+    source .env
+    flask run --host=$HOST --port=$PORT
+fi
